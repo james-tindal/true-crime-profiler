e63824ff57786644b5e1510da58e0e4c5c07a2ad
james-tindal <10291002+james-tindal@users.noreply.github.com>
2026-03-11 15:48:05 +0000

    make pdf background blackish

--- src/utils/pdfExport.js
+++ src/utils/pdfExport.js
@@ -12,6 +12,8 @@ export const exportToPdf = async (selector, filename) => {
   
   const imgData = canvas.toDataURL('image/jpeg', 0.98)
   const pdf = new jsPDF({ unit: 'mm', format: 'a4', orientation: 'portrait' })
+  pdf.setFillColor(10, 10, 10)
+  pdf.rect(0, 0, 210, 297, 'F')
   
   const imgWidth = 190
   const imgHeight = (canvas.height * imgWidth) / canvas.width
